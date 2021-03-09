729fff7baeae627110c3c8394ee5406eff95188f
Lis258 <avb-90@ya.ru>
2021-03-09 22:16:40 +0300

    OrderCardTest.java corrected

--- src/test/java/ru/netology/order/OrderCardTest.java
+++ src/test/java/ru/netology/order/OrderCardTest.java
@@ -8,13 +8,42 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class OrderCardTest {
     @Test
-    void shouldSubmitRequest() {
+    void shouldSubmitRequestValidData() {
         open("http://localhost:9999");
         $("[data-test-id = name] input").setValue("Иванов Иван");
         $("[data-test-id = phone] input").setValue("+79001236987");
         $("[data-test-id = agreement]").click();
         $("[type = button]").click();
         $("[data-test-id = order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
     }
+
+    @Test
+    void shouldSubmitRequestNoData() {
+        open("http://localhost:9999");
+        $("[data-test-id = name] input").setValue("");
+        $("[data-test-id = phone] input").setValue("");
+        $("[type = button]").click();
+        $(".input_invalid[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSubmitRequestInvalidName() {
+        open("http://localhost:9999");
+        $("[data-test-id = name] input").setValue("Ivanov Ivan");
+        $("[data-test-id = phone] input").setValue("+79001236987");
+        $("[data-test-id = agreement]").click();
+        $("[type = button]").click();
+        $(".input_invalid[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSubmitRequestInvalidPhoneNumber() {
+        open("http://localhost:9999");
+        $("[data-test-id = name] input").setValue("Иванов Иван");
+        $("[data-test-id = phone] input").setValue("+790012369877777777");
+        $("[data-test-id = agreement]").click();
+        $("[type = button]").click();
+        $(".input_invalid[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
 }
